ced731d9fa6c5ba17c937fcbf4408b0020d6534c
Konyaz <80565760+Konyaz@users.noreply.github.com>
2022-01-24 00:03:15 +0300

    add

--- src/test/java/tests/ReqresTests.java
+++ src/test/java/tests/ReqresTests.java
@@ -1,5 +1,9 @@
+package tests;
+
+import io.qameta.allure.restassured.AllureRestAssured;
 import org.junit.jupiter.api.Test;
 
+import static filters.CustomLogFilter.customLogFilter;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.*;
@@ -9,12 +13,16 @@ public class ReqresTests {
     @Test
     void registerSuccessful() {
         given()
+                .filter(customLogFilter().withCustomTemplates())
                 .contentType(JSON)
                 .body("{\"email\": \"eve.holt@reqres.in\"," +
                         "\"password\": \"pistol\"}")
                 .when()
+                .log().uri()
+                .log().body()
                 .post("https://reqres.in/api/register")
                 .then()
+                .log().body()
                 .statusCode(200)
                 .body("token", is("QpwL5tke4Pnpja7X4"));
     }
@@ -22,12 +30,16 @@ public class ReqresTests {
     @Test
     void loginSuccessful() {
         given()
+                .filter(customLogFilter().withCustomTemplates())
                 .contentType(JSON)
                 .body("{\"email\": \"eve.holt@reqres.in\"," +
                         "\"password\": \"pistol\"}")
                 .when()
+                .log().uri()
+                .log().body()
                 .post("https://reqres.in/api/login")
                 .then()
+                .log().body()
                 .statusCode(200)
                 .body("token", is("QpwL5tke4Pnpja7X4"));
     }
@@ -35,12 +47,16 @@ public class ReqresTests {
     @Test
     void loginUnSuccessful() {
         given()
+                .filter(customLogFilter().withCustomTemplates())
                 .contentType(JSON)
                 .body("{\"email\": \"eve.holt@reqres.in\"," +
                         "\"password\": \"\"}")
                 .when()
+                .log().uri()
+                .log().body()
                 .post("https://reqres.in/api/login")
                 .then()
+                .log().body()
                 .statusCode(400)
                 .body("error", is("Missing password"));
     }
@@ -82,7 +98,7 @@ public class ReqresTests {
                 .then()
                 .statusCode(200)
                 .body("name", is("morpheus")
-                        , "updatedAt", containsString("2021"));
+                        , "updatedAt", containsString("2022"));
     }
 
     @Test
